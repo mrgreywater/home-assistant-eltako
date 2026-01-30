6636e24b1460968cee1be9b26eec234efe810dab
mrgreywater <mr.greywater@googlemail.com>
2026-01-30 13:02:07 +0100

    fix gateway id parsing homeassistant  | ValueError: invalid literal for int() with base 10: '1, BaseId: FF-E3-E1-00'

--- custom_components/eltako/config_helpers.py
+++ custom_components/eltako/config_helpers.py
@@ -175,7 +175,7 @@ def get_device_name(dev_name: str, dev_id: AddressExpression, general_config: di
         return dev_name
     
 def get_id_from_gateway_name(dev_name: str) -> AddressExpression:
-    return int(dev_name.split('(Id: ')[1].split(')')[0])
+    return int(dev_name.split('(Id: ')[1].split(')')[0].split(',')[0])
     
 
 def get_identifier(gateway_id: int, dev_id: AddressExpression | bytes, event_id:str=None, description_key:str=None) -> str:
